067dfcc89830ebdbb3bcc013804990f7958d63b1
snehanshu123 <124039149+snehanshu123@users.noreply.github.com>
2023-02-01 19:49:33 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from os import environ,getcwd,path
-from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
-
-load_dotenv(path.join(getcwd(),'.env'))
-
-SECRET_KEY = environ.get('SECRET_KEY')
-db=SQLAlchemy()
